4985efd36de91613acbb95bb9f61f015225b72e7
ve2cuy <alain.boudreault@me.com>
2024-06-26 12:00:04 -0400

    First commit

---
+++ blink02/Core/Src/LCDI2C.cpp
@@ -0,0 +1,18 @@
+/*
+ * LCDI2C.cpp
+ *
+ *  Created on: Jun 19, 2024
+ *      Author: alin_
+ */
+
+#include "LCDI2C.h"
+
+LCD_I2C::LCD_I2C() {
+	// TODO Auto-generated constructor stub
+
+}
+
+LCD_I2C::~LCD_I2C() {
+	// TODO Auto-generated destructor stub
+}
+
